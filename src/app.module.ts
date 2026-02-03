26df5abd6fdbbdc2fffb06be1e57185683b9749b
Valentin Fortunato <valenfortunato06@gmail.com>
2026-02-03 17:45:05 -0300

    Demo-NestJs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { User } from './users/users.entity';
+import { Todo } from './todos/todos.entity';
 // import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 // import { AuthGuard } from './guards/auth.guards';
 
 @Module({
-  imports: [UsersModule, TodosModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: "./.env.development",
+    }),
+    TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: "postgres",
+          database: configService.get("DB_NAME"),
+          host: configService.get("DB_HOST"),
+          port: configService.get("DB_PORT"),
+          username: configService.get("DB_USERNAME"),
+          password: configService.get("DB_PASSWORD"),
+          entities: [User, Todo],
+          synchronize: true,
+          logging: true,
+        }),
+    }),
+    UsersModule,
+    TodosModule],
   controllers: [],
   providers: [
     // {
